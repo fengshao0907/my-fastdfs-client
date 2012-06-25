f5bc2cb1101276b20d5fefd01edb5938aa8b3d9a
happyfish100 <happyfish100@1d096adc-f109-6364-288f-2342ab229126>
2012-06-25 03:22:52 +0000

    add java client

---
+++ trunk/java/src/org/csource/myfastdfs/MyFastDFSClient.java
@@ -0,0 +1,26 @@
+/*** Copyright (C) 2012 Happy Fish / YuQing
+  My FastDFS Java Client may be copied only under the terms of the GNU Lesser
+  General Public License (LGPL). 
+  Please visit the FastDFS Home Page http://www.csource.org/ for more detail.
+*/
+
+package org.csource.myfastdfs;
+
+import org.csource.fastdfs.StorageClient1;
+import org.csource.fastdht.FastDHTClient;
+
+/*** My FastDFS client
+* @author Happy Fish / YuQing
+* @version Version 1.00
+*/
+public class MyFastDFSClient
+{
+	public MyFastDFSClient()
+	{
+	}
+
+	public static init(String fdfsConfigFilename, String fdhtConfigFilename) throws FileNotFoundException, IOException, MyException
+	{
+	}
+}
+
